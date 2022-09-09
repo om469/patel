b0b103a98521e8fcda0d7707357f86705d83bde0
Om <ompate8910@.com>
2022-09-09 08:48:20 +0530

    caluculation

---
+++ largest3number.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class largest3number{
+    public static void main(String args[]){
+     Scanner sc=new Scanner(System.in);
+     int A= sc.nextInt();
+     int B= sc.nextInt();
+     int C= sc.nextInt();
+
+     if ((A >= B) && (A>=C)){
+        System.out.println("A");
+     }
+     else if (B>= C){
+        System.out.println("B");
+     }
+     else{
+        System.out.println("C");
+     }
+    }
+    
+}
---
+++ printn.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class printn{
+public static void main(String args[]){
+    Scanner sc=new Scanner (System.in);
+    int range = sc.nextInt();
+    int counter = 1;
+while(counter <= range){
+    System.out.print(counter + " ");
+    counter++;
+}
+ }
+}
